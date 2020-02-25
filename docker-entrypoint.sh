ee673403dd2c4ac3a221cedd91fbe529a586be44
Jeferson Moura <jefmoura@users.noreply.github.com>
2020-02-25 13:03:58 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# It is responsability of the deployment orchestration to execute before
-# migrations, create default admin user, populate minimal data, etc.
+python manage.py migrate
 
 gunicorn kpi_service.wsgi --config kpi_service/gunicorn_conf.py
